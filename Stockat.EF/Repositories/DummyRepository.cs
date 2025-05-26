3cd775d7de929419f1b8bebc07c5a1335bb954f5
mo7amednasr72 <mohamednasr7222@gmail.com>
2025-05-27 01:06:13 +0300

    chore: bootstrap project structure with initial folders and classes and .gitignore file

---
+++ Stockat.EF/Repositories/DummyRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stockat.EF.Repositories
+{
+    internal class DummyRepository
+    {
+    }
+}
