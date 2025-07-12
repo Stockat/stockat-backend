8af56dc90c6a7303ce1e90d074f799b0471c8d2b
Ali <Ali@DESKTOP-HT66DTE>
2025-07-12 21:35:20 +0300

    feat : Audit Table

---
+++ Stockat.API/Controllers/AuditController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Stockat.Core.IServices;
+using Stockat.Core;
+
+namespace Stockat.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuditController : ControllerBase
+    {
+
+        private readonly ILoggerManager _logger;
+        private readonly IServiceManager _serviceManager;
+
+
+        public AuditController(ILoggerManager logger, IServiceManager serviceManager)
+        {
+            _logger = logger;
+            _serviceManager = serviceManager;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> getAllOrdersAudit()
+        {
+
+            var res = _serviceManager.orderProductAuditService.getallAsync().Result;
+
+            return Ok(res);
+
+        }
+    }
+}
---
+++ Stockat.Service/Services/OrderProductAuditService.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Stockat.Core;
+using Stockat.Core.DTOs;
+using Stockat.Core.Entities;
+using Stockat.Core.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stockat.Service.Services;
+
+public class OrderProductAuditService : IOrderProductAuditService
+{
+    private readonly ILoggerManager _logger;
+    private readonly IMapper _mapper;
+    private readonly IRepositoryManager _repo;
+
+    public OrderProductAuditService(ILoggerManager logger, IMapper mapper, IRepositoryManager repo)
+    {
+        _logger = logger;
+        _mapper = mapper;
+        _repo = repo;
+    }
+
+    public async Task<GenericResponseDto<IEnumerable<OrderProductAudit>>> getallAsync()
+    {
+        var res = await _repo.OrderProductAudit.GetAllAsync();
+        return new GenericResponseDto<IEnumerable<OrderProductAudit>>()
+        {
+
+            Data = res,
+            Message = "Data Fetched Successfully",
+            Status = 200,
+        };
+    }
+}
